6b1c78c957b4bee38b5abd2108a5070f113f5c05
Cooper Standard <cooper@cooperstandard.org>
2025-10-14 18:30:12 -0700

    logest substring

--- go/main.go
+++ go/main.go
@@ -4,3 +4,22 @@ func main() {
 	// input := []int{7, 7}
 }
 
+func lengthOfLongestSubstring(s string) int {
+	longest := 0
+	current := 0
+	seen := make(map[rune]bool)
+	for _, c := range s {
+		if !seen[c] {
+			seen[c] = true
+			current += 1
+		} else {
+			current = 1
+			seen = make(map[rune]bool)
+			seen[c] = true
+		}
+		longest = max(current, longest)
+	}
+
+	return longest
+}
+
